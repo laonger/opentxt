577d6ebf414500527caf506035f0e91088badf63
la.onger <yuelang85@gmail.com>
2009-03-21 21:54:07 +0800

    version 0.2 添加了自动生成编码列表的工具，和给编码列表排序的工具。 	增加支持了如下编码： 	-1677721344	utf-32LE 	-1744830208	utf-32BE 	-1811939072	utf-16LE 	-1879047936	UTF-16BE 	-1946156800	UTF-32 	-2147480574	西文（EBCDIC　Latin　1） 	-2147480575	西文（EBCDICUS） 	-2147481079	繁体中文（Big　5-E） 	-2147481082	繁体中文（Big　5　HKSCS） 	-2147481083	简体中文（HZ　GB2312） 	-21474810842147483133	西文（ISOLatin3） 	-2147483614	花饰（MacOS) 	-21474中文（EUC） 	-2147482062	中文（GB　18030） 	-2147482063	中文（GBK) 	-2147482589	繁体中文（Windows，DOS） 	-2147482591	简体中文（Windows，DOS) 	-2147482597	俄文（DOS） 	-2147482606	中欧语系（DOS　Latin　2） 	-2147482608	西文（DOSLatin1） 	-2147482624	美国拉丁文（DOS） 	-2147483121	西文（ISOLatin9） 	-2147483132	中欧语系（ISO　Latin　4） 	-2147483133	西文（ISOLatin3） 	-2147483614	花饰（MacOS) 	-2147483619	中欧语系（MacOS) 	-2147483622	藏文（MacOS) 	-2147483623	简体中文（MacOS) 	-2147483639	梵文（MacOS) 	-2147483646	繁体中文（MacOS） 	1	西文（ASCII） 	10	UTF-16 	12	西文（WindowsLatin1） 	15	中欧语系（Windows　Latin　2） 	2	西文（NextStep) 	30	西文（MacOSRoman） 	4	UTF-8 	5	西文（ISOLatin1） 	6	符号（MacOS) 	7	无损ASCII 	9	中欧语系（ISO　Latin　2）

--- OpenTXT.py
+++ OpenTXT.py
@@ -2,16 +2,22 @@
 # encoding: utf-8
 
 import os, sys, chardet, string
-print sys.argv[0]
-print sys.argv[1]
+#print sys.argv[0]
+#print sys.argv[1]
 
-encodings={'GB2312': '-2147482062', 'utf-8': '4', 'HZ-GB-2312': '-2147481083    '}
+encodings={'GB2312':'-2147482062', 'utf-8':'4', 'HZ-GB-2312':'-2147481083',\
+		'ISO-8859-2':'-1677721344', 'UTF-32BE':'-1946156800', 'EUC-TW':'-2147481295',\
+		'Big5':'-2147483646', 'IBM855':'-2147480575', 'ascii':'1', 'UTF-16BE':'10',\
+		'NONE':'-1744830208'}
 
 file = open(sys.argv[1],'r')
 str = string.join(file.readlines())
 str_encoding = chardet.detect(str)['encoding']
-print str_encoding
-str_encoding=encodings[str_encoding]
-print str_encoding
-#os.popen('open TextEdit')
-os.popen('''/Applications/TextEdit.app/Contents/MacOS/TextEdit -PlainTextEncoding '('''+str_encoding+''')' "'''+sys.argv[1]+'''"''')
+if str_encoding == None:
+	str_encoding = 'None'
+else:
+	print str_encoding
+	str_encoding=encodings[str_encoding]
+	print str_encoding
+	#os.popen('open TextEdit')
+	os.popen('''/Applications/TextEdit.app/Contents/MacOS/TextEdit -PlainTextEncoding '('''+str_encoding+''')' "'''+sys.argv[1]+'''"''')
